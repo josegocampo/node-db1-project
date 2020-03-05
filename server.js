836d757fab93c97b8fae55dcdc0d9386e23c0463
josegocampo <jose.ocampo@mail.udp.cl>
2020-03-05 13:17:39 -0300

    setting up the files

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const server = express();
+const accountsRouter = require('./routes/accountsRouter')
+
 
 const db = require('./data/dbConfig.js');
 
-const server = express();
 
 server.use(express.json());
+
+server.use('/accounts', accountRouter);
+
+
+server.use((err, req, res, next) => {
+        res.startus(500).json({ message: 'There was an internal Server Error'})
+})
+
+
+
+
+
+
+
 
 module.exports = server;
